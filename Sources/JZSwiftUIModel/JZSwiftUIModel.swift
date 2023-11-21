cc7e26cc110bffafb663f4cc67c9a3b1ca937f1e
jiazuo.shao <shaojiazuo@163.com>
2023-11-21 11:03:34 +0800

    新建工程

--- Sources/JZSwiftUIModel/JZSwiftUIModel.swift
+++ Sources/JZSwiftUIModel/JZSwiftUIModel.swift
@@ -1,2 +1,30 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+// 使用Kingfisher的
+public struct JZSmallModule<Base> {
+    public let base: Base
+    public init(_ base: Base) { self.base = base }
+}
+
+public extension Scene {
+    var jz: JZSmallModule<Self> { .init(self) }
+}
+
+public extension View {
+    var jz: JZSmallModule<Self> { .init(self) }
+}
+
+public extension AnyTransition {
+    static var jz: JZSmallModule<Self> { JZSmallModule(.identity) }
+}
+
+public extension JZSmallModule where Base == Any {
+    init(_ content: Base) { self.base = content }
+}
+
+public extension NSObjectProtocol {
+    var jz: JZSmallModule<Self> { .init(self) }
+}
--- Sources/JZSwiftUIModel/ViewExtension/JZChange.swift
+++ Sources/JZSwiftUIModel/ViewExtension/JZChange.swift
@@ -7,40 +7,25 @@
 
 import SwiftUI
 
-public extension JZSmallModule where Base == any View {
+public extension JZSmallModule where Base: View {
     func onChange<V>(of value: V, _ action: @escaping (_ oldValue: V, _ newValue: V) -> Void) -> some View where V : Equatable {
-//#if os(macOS)
-//        if #available(macOS 14.0, *) {
-//            return AnyView(base).onChange(of: value, initial: false) { oldValue, newValue in
-//                action(oldValue, newValue)
-//            }
-//        }else {
-//            return AnyView(base).onChange(of: value) { newValue in
-//                action(value, newValue)
-//            }
-//        }
-//#else
-//        if #available(iOS 17.0, *) {
-//            return AnyView(base).onChange(of: value, initial: false) { oldValue, newValue in
-//                action(oldValue, newValue)
-//            }
-//        }else {
-//            return AnyView(base).onChange(of: value) { newValue in
-//                action(value, newValue)
-//            }
-//        }
-//#endif
         if #available(iOS 17.0, macOS 14.0, *) {
-            return AnyView(base).onChange(of: value, initial: false) { oldValue, newValue in
+            return base.onChange(of: value, initial: false) { oldValue, newValue in
                 action(oldValue, newValue)
             }
         }else {
-            return AnyView(base).onChange(of: value) { newValue in
+            return base.onChange(of: value) { newValue in
                 action(value, newValue)
             }
         }
     }
 }
+
+
+
+
+
+
 
 
 
